1ab2a3215e7f8d754635d94b14703df5b7af27ac
Erlend Foss <erlend.foss@ice.net>
2020-04-24 09:58:27 +0200

    Update README and script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mib2yaml
 Translate MIB files to YAML files to use them with Logstash SNMP trap plugin
+## Usage
+Put the names of the MIBs you want to translate into the mibs array (replace the MIB names I have put there as an example). The MIB files must exist in a directory you have assigned for MIBs, usually /usr/share/snmp/mibs.
+The output files are stored in /tmp, for example /tmp/NSN-SNMP-NBI-COMMONFUNCTIONS-MIB.yaml
+To use the yaml files with Logstash, copy the files to /usr/share/logstash/vendor/bundle/jruby/2.5.0/gems/snmp-1.3.1/data/ruby/snmp/mibs/
+(this directory name may vary according to your Logstash version. This is for Logstash 6.8)
---
+++ mib2yaml.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Put the names of the MIBs you want to translate into the mibs array. The MIB files must exist in a directory you have assigned for MIBs, usually /usr/share/snmp/mibs
+# The output files are stored in /tmp, for example /tmp/NSN-SNMP-NBI-COMMONFUNCTIONS-MIB.yaml
+# To use the yaml files with Logstash, copy the files to /usr/share/logstash/vendor/bundle/jruby/2.5.0/gems/snmp-1.3.1/data/ruby/snmp/mibs/
+# (this directory name may vary according to your Logstash version. This is for Logstash 6.8)
+
+declare -a mibs=("NSN-SNMP-NBI-COMMONFUNCTIONS-MIB" "NSN-SNMP-NBI-REGISTRATION-MIB" "NSN-SNMP-NBI-TOPOLOGY-MIB" "NSN-SNMP-NBI-FAULTMANAGEMENT-MIB" "NSN-SNMP-NBI-TEXTUALCONVENTIONS-MIB" "SYSAPPL-MIB")
+
+for mib in "${mibs[@]}"
+do
+        snmptranslate -Ts -Pu -m $mib | awk -F"." '{print $NF}' >> /tmp/$mib.s
+        snmptranslate -To -Pu -m $mib | sed 's/^\./: /g' >> /tmp/$mib.o
+        paste -d "" /tmp/$mib.s /tmp/$mib.o >> /tmp/$mib.yaml
+done
